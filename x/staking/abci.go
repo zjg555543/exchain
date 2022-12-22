dfdf15e97c2fcb5578f5806197dad8b60dc3043e
JianGuo <zjg555543@163.com>
2022-12-22 17:44:21 +0800

    Update abci.go

--- x/staking/abci.go
+++ x/staking/abci.go
@@ -5,7 +5,7 @@ import (
 	"github.com/okex/exchain/libs/tendermint/types"
 )
 
-// BeginBlocker will persist the current header and validator set as a historical entry
+// BeginBlocker will persist the current header and validator set as a historical entry,test
 // and prune the oldest entry based on the HistoricalEntries parameter
 func BeginBlocker(ctx sdk.Context, k Keeper) {
 	if types.HigherThanVenus1(ctx.BlockHeight()) {
